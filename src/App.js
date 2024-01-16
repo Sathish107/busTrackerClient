f9f079f916f09b82148542bd25aae0133b3ba2c1
Balasanthosh A <maarimuthu107@gmail.com>
2024-01-16 16:58:51 +0530

    added fullscreen api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import Home from "./Home/Home"
+import { useEffect } from "react"
 
 const App=()=>{
+    useEffect(()=>{
+        if (document.documentElement.requestFullscreen) {
+            document.documentElement.requestFullscreen();
+        } else if (document.documentElement.mozRequestFullScreen) { // Firefox
+            document.documentElement.mozRequestFullScreen();
+        } else if (document.documentElement.webkitRequestFullscreen) { // Chrome, Safari and Opera
+            document.documentElement.webkitRequestFullscreen();
+        } else if (document.documentElement.msRequestFullscreen) { // IE/Edge
+            document.documentElement.msRequestFullscreen();
+        }   
+    },[])
+    
     return(
         <div className="App">
             <Home />
