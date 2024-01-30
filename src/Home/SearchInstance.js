14ddfa53d5d5826cc02c86ca6f327fc017edcb2f
Balasanthosh A <maarimuthu107@gmail.com>
2024-01-30 21:13:40 +0530

    added limit to the search instance paragraph

--- src/Home/SearchInstance.js
+++ src/Home/SearchInstance.js
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom"
 
 const SearchInstance=({id,start,destination})=>{
+
     const navigate=useNavigate()
     return(
         <>
@@ -9,9 +10,9 @@ const SearchInstance=({id,start,destination})=>{
                 <div key={id} onClick={()=>{
                     navigate(`/route/${id}`)
                 }}>
-                    <p>{start}</p>
+                    <p>{(start.length > 11)?start.slice(0,11)+"...":start}</p>
                     <p>to</p>
-                    <p>{destination}</p>
+                    <p>{(destination.length > 11)?destination.slice(0,11)+"...":destination}</p>
                 </div>:
                 <p>Loading...</p>
             }
